5a2b39898f77d960efa577f39c1493ca811ee570
daldeluca <dalesio@umich.edu>
2025-03-11 19:27:10 -0400

    Update Turn.h

--- Turn.h
+++ Turn.h
@@ -2,16 +2,22 @@
 #ifndef TURN_H
 #define TURN_H
 #include "Die.h"
-#include "pgame.h"
+
 using namespace std;
 
 class Turn {
 private:
-    GameState& game;
-    Die& die;
+    Die myDie;
+    int score_this_turn;
+    int turn_count;
+    bool turn_over;
+    char choice;
 
     public:
-    Turn(GameState& game, Die& die);
+    Turn();
+    int get_score_this_turn();
+    int get_turn_count();
     void take_turn();
+    void reset_turn_score();
 };
 #endif //TURN_H
