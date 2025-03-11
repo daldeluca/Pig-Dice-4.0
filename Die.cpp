4cbd1737e68516e3818c768c1d4e1297b4cf42a7
daldeluca <dalesio@umich.edu>
2025-03-11 19:25:01 -0400

    Update Die.cpp

--- Die.cpp
+++ Die.cpp
@@ -1,10 +1,16 @@
 #include "Die.h"
 #include <cstdlib>
+#include <ctime>
+#include <bits/locale_classes.h>
 using namespace std;
 
 Die::Die(int num_sides) : num_sides(num_sides), value(1) {}
 
-int Die::roll() {
+void Die::roll() {
+  srand(time(0));
   value = rand() % num_sides + 1;
+}
+
+int Die::get_value() {
   return value;
 }
